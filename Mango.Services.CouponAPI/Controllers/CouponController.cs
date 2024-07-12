dbdab4e2d9366d1ea5e967634b37eafd91243896
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2024-07-12 16:22:34 +0530

    First commit

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,60 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly AppDbContext _dbcontext;
+        
+        public CouponController(AppDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _dbcontext.Coupons.First(c => c.CouponId == id);
+                if (coupon != null)
+                {
+                    return Ok(coupon);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = ex.Message }
+                );
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_dbcontext.Coupons.ToList());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = ex.Message }
+                );
+            }
+        }
+    }
+}
