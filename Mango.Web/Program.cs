781b1b608795491e4c14225d57e8e4b5713ce7c2
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2024-07-14 15:11:03 +0530

    Fixed program.cs

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -9,9 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
-builder.Services.AddScoped<IBaseService, BaseService>();
-builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddHttpClient<ICouponService, CouponService>();
+//builder.Services.AddHttpClient<IProductService, ProductService>();
+//builder.Services.AddHttpClient<ICartService, CartService>();
+//builder.Services.AddHttpClient<IOrderService, OrderService>();
+Constant.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
+Constant.OrderAPIBase = builder.Configuration["ServiceUrls:OrderAPI"];
+Constant.ShoppingCartAPIBase = builder.Configuration["ServiceUrls:ShoppingCartAPI"];
+Constant.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
+Constant.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+builder.Services.AddScoped<ITokenProvider, TokenProvider>();
+builder.Services.AddScoped<IBaseService, BaseService>();
+//builder.Services.AddScoped<IOrderService, OrderService>();
+//builder.Services.AddScoped<IProductService, ProductService>();
+//builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
@@ -19,8 +33,6 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.LoginPath = "/Auth/Login";
         options.AccessDeniedPath = "/Auth/AccessDenied";
     });
-
-Constant.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 
 var app = builder.Build();
 
@@ -38,10 +50,6 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
